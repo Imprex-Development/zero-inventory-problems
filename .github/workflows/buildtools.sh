425ef2bfdc5288f7d40a8bb85593580f5e4560c6
Nils Gereke <nilsgereke1@gmail.com>
2023-03-17 22:36:14 +0100

    Support 1.19.4 (#6)
    * feat: added 1.19.4 support
    * feat: removed codemc
    * feat: added buildtools for self building nms dependencies

---
+++ .github/workflows/buildtools.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+# Source: https://github.com/Imprex-Development/orebfuscator/blob/master/.github/workflows/buildtools.sh
+
+build () {
+	JAVA_PATH=$"JAVA_HOME_$2_X64"
+	export JAVA_HOME=${!JAVA_PATH}
+
+	echo "Building v$1 with java-$2 ($JAVA_HOME)"
+
+	rm -rf $1
+    mkdir $1
+    cd $1
+
+    curl -o BuildTools.jar https://hub.spigotmc.org/jenkins/job/BuildTools/lastSuccessfulBuild/artifact/target/BuildTools.jar
+    "$JAVA_HOME/bin/java" -jar BuildTools.jar --rev $1 --remapped
+
+    cd ..
+}
+
+checkVersion () {
+	echo Checking version $1
+
+	if [ ! -d ~/.m2/repository/org/spigotmc/spigot/$1-R0.1-SNAPSHOT ]; then
+		build $1 $2
+	fi
+}
+
+checkVersion "1.19"   "17"
+checkVersion "1.19.3" "17"
+checkVersion "1.19.4" "17"
